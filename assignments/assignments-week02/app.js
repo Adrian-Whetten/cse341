76da2a239c78fb36c8ad8c570547576260d3c5b2
Adrian Whetten <46587336+Adrian-Whetten@users.noreply.github.com>
2022-01-17 17:49:52 -0700

    assignment completion

--- assignments/assignments-week02/app.js
+++ assignments/assignments-week02/app.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 
-const rockRoutes = require('./routes/rockstar.js');
-const teachRoutes = require('./routes/teacher.js');
-const notFoundRoute = require('./routes/404.js');
+const users = [];
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use('/users', rockRoutes);
-app.use('/', teachRoutes);
-//app.use(notFoundRoute);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  res.render('index', { pageTitle: 'Add User' });
+});
+
+app.get('/users', (req, res, next) => {
+  res.render('users', {
+    pageTitle: 'User',
+    users: users,
+    hasUsers: users.length > 0
+  });
+});
+
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect('/users');
+});
 
 app.listen(5000);
