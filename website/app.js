08114d8051b0fadd6d644bf7128ff152ec7c36a0
Adrian Whetten <46587336+Adrian-Whetten@users.noreply.github.com>
2022-02-05 16:23:35 -0700

    MongoDB and Mongoose

--- website/app.js
+++ website/app.js
@@ -3,8 +3,10 @@ const PATH = process.env.PORT || 5000;
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,9 +19,39 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById('61fd8b47f5ce59f850571816')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(PATH);
+mongoose
+  .connect(
+    'mongodb+srv://Moondawg32:Moondawg!!04@ecommerce.o3qob.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'James Moon',
+          email: 'jMoon@cool.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PATH);
+  })
+  .catch(err => {
+    console.log(err);
+  });
