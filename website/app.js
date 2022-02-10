42d526496cfc01ff2f30f775ea7386f1e69794c2
Adrian Whetten <46587336+Adrian-Whetten@users.noreply.github.com>
2022-02-10 15:55:12 -0700

    adding heroku

--- website/app.js
+++ website/app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://Moondawg32:Moondawg!!04@ecommerce.o3qob.mongodb.net/shop';
+  'mongodb+srv://Moondawg32:Moondawg!!04@ecommerce.o3qob.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
